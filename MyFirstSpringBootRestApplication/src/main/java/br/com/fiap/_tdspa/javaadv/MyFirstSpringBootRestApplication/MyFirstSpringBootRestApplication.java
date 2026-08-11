0c8aa6a1d8bcd4ded33e6a1c5d8ac82b48837392
Orlando <orlando.patriarcha@gmail.com>
2026-08-11 09:54:15 -0300

    initial repository load

---
+++ MyFirstSpringBootRestApplication/src/main/java/br/com/fiap/_tdspa/javaadv/MyFirstSpringBootRestApplication/MyFirstSpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap._tdspa.javaadv.MyFirstSpringBootRestApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootRestApplication.class, args);
+	}
+
+}
---
+++ MyFirstSpringBootRestApplication/src/test/java/br/com/fiap/_tdspa/javaadv/MyFirstSpringBootRestApplication/MyFirstSpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap._tdspa.javaadv.MyFirstSpringBootRestApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
